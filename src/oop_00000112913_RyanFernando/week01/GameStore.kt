0029d5d641fd5a39bb112dfa2affcc9f3c5b6aa7
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-03 22:10:06 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000112913_RyanFernando/week01/GameStore.kt
+++ src/oop_00000112913_RyanFernando/week01/GameStore.kt
@@ -1,2 +1,15 @@
 package oop_00000112913_RyanFernando.week01
 
+fun main() {
+    val gameTitle = "DYNASTY WARRIORS: ORIGINS"
+    val price = 945000
+
+    val finalPrice = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000)
+        price - (price * 20 / 100)
+    else
+        price - (price * 10 / 100)
+

95be99b314bbd72b23835202bfcfbbf70e4eaab3
Ryan Fernando <ryanfer1888@gmail.com>
2026-02-03 19:32:04 +0700

    week01: add calculateStatus function

--- src/oop_00000112913_RyanFernando/week01/GradingSystem.kt
+++ src/oop_00000112913_RyanFernando/week01/GradingSystem.kt
@@ -12,6 +12,9 @@ fun main() {
         in 70..79 -> "C"
         else -> "D"
     }
-
     println("Grade kamu: $grade")
+
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"

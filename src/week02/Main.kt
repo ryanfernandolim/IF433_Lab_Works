0b5355ddc222519eeac2223d3d95a65dbb044eaf
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-11 02:10:23 +0700

    week02: (task 1) implement library input system

--- src/week02/Main.kt
+++ src/week02/Main.kt
@@ -5,40 +5,25 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("--- APLIKASI PMB UMN ---")
+    print("Masukkan Judul Buku: ")
+    val title = scanner.nextLine()
 
-    print("Masukkan Nama: ")
-    val name = scanner.nextLine()
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
 
-    print("Masukkan NIM (Wajib 5 Karakter): ")
-    val nim = scanner.next()
+    print("Masukkan Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
 
-    scanner.nextLine()
-
-    if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-    } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
-
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+    if (duration < 0) {
+        duration = 1
     }
 
-    print("Pilih Jalur (1.Reguler, 2. Umum): ")
-    val type = scanner.nextInt()
-    scanner.nextLine()
+    val loan = Loan(title, borrower, duration)
+    val fine = loan.calculateFine()
 
-    if (type == 1) {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
-        val s1 = Student(name, nim, major)
-        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
-    } else if (type == 2) {
-        val s2 = Student(name, nim)
-        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
-    } else {
-        println("Pilihan ngawur, pendaftaran batal!")
-    }
-
+    println("--- DETAIL PEMINJAMAN ---")
+    println("Judul Buku   : ${loan.bookTitle}")
+    println("Peminjam    : ${loan.borrower}")
+    println("Lama Pinjam : ${loan.loanDuration} hari")
+    println("Total Denda : Rp $fine")
 }

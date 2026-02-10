146ed911bfcfded41a6c591fbd68dfa8202b4074
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-11 02:00:16 +0700

    week02: challenge refactor primary constructor using default args

--- src/week02/Student.kt
+++ src/week02/Student.kt
@@ -3,8 +3,8 @@ package oop_00000112913_RyanFernando.week02
 class Student (
     val name: String,
     val nim: String,
-    var gpa: Double = 0.0,
-    var major: String) {
+    var major: String,
+    var gpa: Double = 0.0) {
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
@@ -12,5 +12,8 @@ class Student (
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
+    }
+    constructor(name: String, nim: String) : this(name, nim, "Non-marticulated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }

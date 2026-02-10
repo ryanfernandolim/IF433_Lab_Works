9c290d67c51d81f9c8b3a8e4657a36776fd2cf1d
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-11 02:09:24 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_00000112913_RyanFernando.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}

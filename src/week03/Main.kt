b01aa536c4b1886ecbe0cf87a1cef1ee87da7a8e
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-24 09:00:48 +0700

    week03: (task 2) implement Player testing in main

--- src/week03/Main.kt
+++ src/week03/Main.kt
@@ -11,13 +11,17 @@ fun main() {
     println("Pajak yang harus dibayar: ${e.tax}")
 
     println("\n=== WEAPON TEST ===")
-
     val sword = Weapon("Dragon Slayer", 100)
-
     sword.damage = -50      // harus gagal
     sword.damage = 9999     // harus jadi 1000
 
     println("Weapon: ${sword.name}")
     println("Damage: ${sword.damage}")
     println("Tier: ${sword.tier}")
+
+    println("\n=== PLAYER TEST ===")
+
+    val player = Player("RyanX")
+    player.addXp(50)   // masih level 1
+    player.addXp(60)   // total 110 → level 2
 }

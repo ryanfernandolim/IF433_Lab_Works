b1a14122de8c871580c379472752af7d5d832897
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-24 08:58:45 +0700

    week03: (task 1) implement Weapon testing in main

--- src/week03/Main.kt
+++ src/week03/Main.kt
@@ -9,4 +9,15 @@ fun main() {
     e.increasePerformance()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n=== WEAPON TEST ===")
+
+    val sword = Weapon("Dragon Slayer", 100)
+
+    sword.damage = -50      // harus gagal
+    sword.damage = 9999     // harus jadi 1000
+
+    println("Weapon: ${sword.name}")
+    println("Damage: ${sword.damage}")
+    println("Tier: ${sword.tier}")
 }

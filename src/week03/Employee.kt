dbea7596035c4d352acaa72e79cb8f98f224111d
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-24 08:28:10 +0700

    week03: fix setter using backing field

--- src/week03/Employee.kt
+++ src/week03/Employee.kt
@@ -2,4 +2,12 @@ package week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
+        }
 }

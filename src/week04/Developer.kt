d45bfcb3b01f53867f1f0218b6079819032b0efe
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-02-25 15:34:38 +0700

    week04: (task 2) create Developer class

---
+++ src/week04/Developer.kt
@@ -0,0 +1,8 @@
+package week04
+
+class Developer(name: String, baseSalary: Int, val programmingLanguage: String) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+}

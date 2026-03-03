187dacf70fddd587814b539bcc966675ec70f41a
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-03 22:42:38 +0700

    week05: (task 1) create MathHelper class with overloading

--- src/week05/Main.kt
+++ src/week05/Main.kt
@@ -1,2 +1,30 @@
 package week05
 
+fun main() {
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+    val math = MathHelper()
+
+    println("Luas Persegi: ${math.hitungLuas(4)}")
+    println("Luas Persegi Panjang: ${math.hitungLuas(5, 3)}")
+    println("Luas Lingkaran: ${math.hitungLuas(7.0)}")
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn}")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("--------------------------")
+    }
+}
--- src/week05/MathHelper.kt
+++ src/week05/MathHelper.kt
@@ -1,2 +1,16 @@
 package week05
 
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}

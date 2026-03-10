f79700ade62da5a7c8c53a876c07486e1a665966
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-10 18:32:08 +0700

    wekk06: fix diamond problem using explict super delegation

--- src/week06/Smartphone.kt
+++ src/week06/Smartphone.kt
@@ -1,3 +1,9 @@
 package week06
 
-class Smartphone : Camera, Phone {}
+class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
+}

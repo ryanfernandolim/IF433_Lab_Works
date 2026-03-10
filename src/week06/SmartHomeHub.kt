ed19d9b5b2e7daa1345a882560052aa6efa62ce4
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-10 18:50:31 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/week06/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+}

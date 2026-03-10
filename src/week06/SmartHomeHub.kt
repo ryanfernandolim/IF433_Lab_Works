eebf6a81f095a25e63499f0a49708274c14022f6
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-10 18:52:10 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/week06/SmartHomeHub.kt
+++ src/week06/SmartHomeHub.kt
@@ -16,4 +16,17 @@ class SmartHomeHub {
         }
     }
 
+    fun activateSecurityMode() {
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+
+        }
+    }
 }

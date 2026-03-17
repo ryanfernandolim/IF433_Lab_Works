d7aebe700c010ce176e7ba66d9e560dce753768d
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-17 20:15:49 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/week07/Main.kt
+++ src/week07/Main.kt
@@ -27,6 +27,16 @@ fun main() {
     val (userName, userAge) = data1
     println("Destructured: $userName berumur $userAge")
 
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+    val uiMessage = when (response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Sedang memuat..."
+    }
+
+    println(uiMessage)
 
 
 }

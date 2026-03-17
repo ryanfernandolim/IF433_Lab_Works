cf6765c144a10e4ce93868df167893126de45efd
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-17 20:21:51 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/week07/GameExecutor.kt
@@ -0,0 +1,10 @@
+package week07
+
+fun processEvent(event: BattleState) {
+    when (event) {
+        is BattleState.MonsterEncounter -> println("Monster muncul: ${event.monsterName}")
+        is BattleState.LootDropped -> println("Loot didapat: ${event.item.name} (${event.item.rarity})")
+        is BattleState.GameOver -> println("Game Over: ${event.reason}")
+        BattleState.SafeZone -> println("Pemain berada di Safe Zone.")
+    }
+}

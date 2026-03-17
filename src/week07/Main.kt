34404a6023cc35a77c497d31ab7f5c9b76227601
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-03-17 20:04:29 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/week07/Main.kt
@@ -0,0 +1,7 @@
+package week07
+
+fun main() {
+
+    val client = NetworkClient("https://api.umn.ac.id")
+
+}
---
+++ src/week07/NetworkClient.kt
@@ -0,0 +1,9 @@
+package week07
+
+class NetworkClient private constructor(val url: String) {
+
+    fun connect() {
+        println("Connecting to $url...")
+    }
+
+}

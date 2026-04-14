9532b1ffa184856ad2ac71486f75c84c8cfc7ebe
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-04-14 14:52:40 +0700

    week08: implement smart cast in function

---
+++ src/Week08/NotificationService.kt
@@ -0,0 +1,15 @@
+package Week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        if (user.email != null) {
+            sendEmail(user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email")
+        }
+    }
+}

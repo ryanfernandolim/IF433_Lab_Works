3e178241d14b99ef6391d4a6c5346aac4f3b5306
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-04-14 15:20:23 +0700

    week08: implement java interop using !!

--- src/Week08/Main.kt
+++ src/Week08/Main.kt
@@ -53,4 +53,9 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Lenght: $statusLength)")
 }

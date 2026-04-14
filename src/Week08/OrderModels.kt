83d1556c6d4f61f9bb403c9ec087ef0a7a1d975e
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-04-14 15:00:44 +0700

    week08: implement safe call with let block

--- src/Week08/OrderModels.kt
+++ src/Week08/OrderModels.kt
@@ -3,4 +3,4 @@ package Week08
 class City(val name: String)
 class Address(val city: City)
 class DeliveryDetails(val address: Address?)
-class Order(val deliveryDetails: DeliveryDetails>, val totalPrice: Int?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)

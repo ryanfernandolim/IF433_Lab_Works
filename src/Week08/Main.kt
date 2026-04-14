cb480d3552a1117c8331c4c4181e6a3213f1643b
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-04-14 16:58:18 +0700

    week08: (task) process mixed raw JSON list using as?

--- src/Week08/Main.kt
+++ src/Week08/Main.kt
@@ -60,4 +60,30 @@ fun main() {
     println("Status dari Java: $javaResponse (Lenght: $statusLength)")
 
     runMockUnitTest()
+
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"), // Corrupted warranty
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+            product?.let {
+                when (it) {
+                    is Electronic -> println("Produk: ${it.name} | Warranty: ${it.warrantyMonths} bulan")
+                    is Clothing -> println("Produk: ${it.name} | Size: ${it.size}")
+                }
+                parser.checkout(it)
+            } ?: println("SKIP: Tipe produk tidak dikenali pada data: $raw")
+
+        } catch (e: IllegalArgumentException) {
+            println("DATA KORUP DITANGKAP: ${e.message}")
+        }
+    }
 }

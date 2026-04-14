10b9e85530d165636642b745c2b38259778421b5
Ryan Fernando <ryan.fernando1@student.umn.ac.id>
2026-04-14 15:02:56 +0700

    week08: create mixed data collection

--- src/Week08/Main.kt
+++ src/Week08/Main.kt
@@ -14,4 +14,13 @@ fun main() {
         val tax = price * 0.11
         "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
     } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println("\n=== TEST SAFE CASTING")
+    val mixedData: List<Any> = ListOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }

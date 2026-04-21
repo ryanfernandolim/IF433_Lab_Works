34f762b3f5a21e23420648aee2dee19f622bfea3
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-04-21 09:33:25 +0700

    week09: create immutable and mutable list

---
+++ src/Week09/CollectionBasics.kt
@@ -0,0 +1,12 @@
+package Week09
+
+fun main() {
+    println("=== TEST LIST ===")
+    val frameworks: List<String> = listOf("Kotlin", "Java", "C++")
+    println("Immutable List: $frameworks")
+
+    val scores: MutableList<Int> = mutableListOf(85, 90)
+    scores.add(95)
+    scores[0] = 88
+    println("Mutable List: $scores")
+}

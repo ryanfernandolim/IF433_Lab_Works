d30ac751155910cddce632e22b314fefeeb27e14
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-04-28 13:52:52 +0700

    week10: (task) populate coin repository

--- src/Week10/CryptoDashboard.kt
+++ src/Week10/CryptoDashboard.kt
@@ -3,4 +3,8 @@ package Week10
 fun main() {
     println("=== CRYPTO WALLET DASHBOARD ===")
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.85))
+    coinRepo.add(Coin("ETH", 5.20))
+    coinRepo.add(Coin("USDT", 1500.00))
 }

27aff245a027224f88df3cbab6454f808c3bbc6e
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-04-28 13:53:17 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/Week10/CryptoDashboard.kt
+++ src/Week10/CryptoDashboard.kt
@@ -7,4 +7,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.85))
     coinRepo.add(Coin("ETH", 5.20))
     coinRepo.add(Coin("USDT", 1500.00))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }

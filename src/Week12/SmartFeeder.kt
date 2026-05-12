19c3bf6ef6ae20a8ca8d435699e7ff63e01ec2d8
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-05-12 12:18:18 +0700

    week12: (task) implement food stock validation with throw

--- src/Week12/SmartFeeder.kt
+++ src/Week12/SmartFeeder.kt
@@ -6,5 +6,11 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if (isJammed) {
         throw DispenserJamException()
     }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
 

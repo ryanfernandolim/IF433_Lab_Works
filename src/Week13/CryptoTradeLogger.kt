14feeaacd6e17c679077addcba111b7f351d48ae
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-05-19 15:09:49 +0700

    week13: (task) inject malformed line via appendText to test robustness

--- src/Week13/CryptoTradeLogger.kt
+++ src/Week13/CryptoTradeLogger.kt
@@ -53,4 +53,7 @@ fun main() {
 
     saveTrades(trades, "crypto_trades.csv")
     println("Data trade berhasil disimpan.")
+
+    File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
+    println("Baris korup berhasil diinjeksi.")
 }

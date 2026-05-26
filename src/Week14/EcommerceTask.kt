9128a72f0082f707c2143036411a364f65b5c0b3
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-05-26 15:09:56 +0700

    week14: (task) copy bad E-Commerce OrderProcessor code

---
+++ src/Week14/EcommerceTask.kt
@@ -0,0 +1,18 @@
+package Week14
+
+import java.io.File
+
+class BadOrderProcessor {
+    private val file = File("orders.csv")
+
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType) {
+            "REGULAR" -> basePrice
+            "VIP"     -> basePrice * 0.90
+            else      -> basePrice
+        }
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        file.appendText("$itemName,$finalPrice,$customerType\n")
+        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}

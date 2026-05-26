dff8658eb5db33381c4a9d10972fe0ed863bcfdd
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-05-26 15:09:15 +0700

    week14: create tight coupled UserService (DIP violation)

---
+++ src/Week14/DipViolation.kt
@@ -0,0 +1,10 @@
+package Week14
+
+class MySQLDatabase {
+    fun query(sql: String): List<String> = listOf("data1", "data2")
+}
+
+class UserService {
+    private val database = MySQLDatabase()
+    fun getUser(id: Int) = database.query("SELECT * FROM users WHERE id=$id")
+}

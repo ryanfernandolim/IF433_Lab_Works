ac877ab48a4854e8fc11b9f583d2ab9ff16da385
ryanfernandolim <ryan.fernando1@student.umn.ac.id>
2026-05-26 15:09:33 +0700

    week14: create Database abstraction and implementations

---
+++ src/Week14/DipRefactored.kt
@@ -0,0 +1,9 @@
+package Week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase : Database {
+    override fun query(sql: String) = listOf("pg_data1", "pg_data2")
+}
